65582f91ec31b17341d8d1843cc41261d7885904
João Victor <joaoslx1@gmail.com>
2025-03-11 23:54:49 -0300

    novo blog

---
+++ blog/assets/js/script.js
@@ -0,0 +1,15 @@
+const checkbox = document.getElementById("checkbox")
+checkbox.addEventListener("change", () => {
+  document.body.classList.toggle("dark")
+})
+
+document.addEventListener('DOMContentLoaded', function () {
+  const navLinks = document.querySelectorAll('.nav-link');
+
+  navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      navLinks.forEach(nav => nav.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
